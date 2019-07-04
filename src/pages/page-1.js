c0998b3ad52eb21ed2b61fd880576daf7e029457
jonaskay <jonaskay@iki.fi>
2019-07-04 18:27:14 +0300

    Move default components to a separate folder

--- src/pages/page-1.js
+++ src/pages/page-1.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from "../components/default/layout"
+import Image from "../components/default/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
