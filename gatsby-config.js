2d3543f12c0769559ad8f4d9899e6cc4142c1e21
jonaskay <jonaskay@iki.fi>
2019-07-06 10:54:19 +0300

    Update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Web Font Preview`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Preview Google Fonts on a real website. Experiment with typefaces using templates instead of type speciments.`,
+    author: `@webfontpreview`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
