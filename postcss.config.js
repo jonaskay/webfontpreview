9729d93df918c2264ef738ee821913b92777431f
jonaskay <jonaskay@iki.fi>
2019-07-12 22:52:46 +0300

    Install postcss-preset-env for modern CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
+const postcssPresetEnv = require(`postcss-preset-env`)
+
 module.exports = () => ({
-  plugins: [require("tailwindcss")],
+  plugins: [
+    require("tailwindcss"),
+    postcssPresetEnv({
+      stage: 0,
+    }),
+  ],
 })
