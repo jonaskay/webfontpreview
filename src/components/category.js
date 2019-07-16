79b7dfca59f50e49769a7fc7d1186dee412f28b3
jonaskay <jonaskay@iki.fi>
2019-07-16 21:00:06 +0300

    Change color scheme

--- src/components/category.js
+++ src/components/category.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types"
 
 const Category = ({ name, value, active, onClick }) => {
   const defaultClassName =
-    "inline-block px-3 py-1 mx-1 my-1 border rounded-full cursor-pointer"
+    "inline-block px-3 py-1 mx-1 my-1 border border-gray-700 rounded-full cursor-pointer"
 
   return (
     <span
       className={
         active
-          ? `bg-red-500 text-white border-red-600 ${defaultClassName}`
-          : `border-red-500 text-red-500 ${defaultClassName}`
+          ? `bg-gray-700 text-white ${defaultClassName}`
+          : `text-gray-700 ${defaultClassName}`
       }
       onClick={() => onClick(value)}
     >
